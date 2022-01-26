ee5a04a673e183511cedd36a63dea9679a4c2115
JEREMIAH CURTIS <89932629+JeremiahC0510@users.noreply.github.com>
2022-01-26 16:39:34 -0500

    Created My Image and Coded Into Picture

--- Picture.java
+++ Picture.java
@@ -6,15 +6,17 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Jeremiah Curtis
+ * @version 01.26.2022
  */
 public class Picture
 {
-    private Square wall;
+    private Square house;
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Person guy;
+    private Square ground;
     private boolean drawn;
 
     /**
@@ -22,10 +24,12 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
+        house = new Square();
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        guy = new Person();
+        ground = new Square();
         drawn = false;
     }
 
@@ -35,27 +39,39 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
+            house.moveHorizontal(-50);
+            house.moveVertical(0);
+            house.changeSize(150);
+            house.makeVisible();
             
             window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
+            window.moveHorizontal(35);
+            window.moveVertical(20);
             window.changeSize(40);
             window.makeVisible();
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
+            roof.changeColor("magenta");
+            roof.changeSize(100, 170);
+            roof.moveHorizontal(125);
+            roof.moveVertical(-120);
             roof.makeVisible();
     
             sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
+            sun.moveHorizontal(-190);
+            sun.moveVertical(-60);
+            sun.changeSize(75);
             sun.makeVisible();
+            
+            guy.changeColor("blue");
+            guy.moveHorizontal(-60);
+            guy.moveVertical(20);
+            guy.makeVisible();
+            
+            ground.changeColor("green");
+            ground.moveHorizontal(-320);
+            ground.moveVertical(130);
+            ground.changeSize(520);
+            ground.makeVisible();
             drawn = true;
         }
     }
@@ -65,10 +81,12 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
+        house.changeColor("black");
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
+        guy.changeColor("black");
+        ground.changeColor("black");
     }
 
     /**
@@ -76,9 +94,11 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
+        house.changeColor("red");
         window.changeColor("black");
-        roof.changeColor("green");
+        roof.changeColor("magenta");
         sun.changeColor("yellow");
+        guy.changeColor("blue");
+        ground.changeColor("green");
     }
 }
